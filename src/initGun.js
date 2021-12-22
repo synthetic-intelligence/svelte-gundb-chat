f08d5210c9c5eb390e7e2f22e54b6a3215bf7c2f
synthetic-intelligence <69837543+synthetic-intelligence@users.noreply.github.com>
2021-12-22 12:02:43 -0700

    per pull

--- src/initGun.js
+++ src/initGun.js
@@ -1,5 +1,13 @@
 import Gun from "gun/gun";
 import "gun/lib/webrtc";
+import "gun/lib/radix";
+import "gun/lib/radisk";
+import "gun/lib/rindexed";
+// using store doesn't work, fs not supported
+// is this required for indexedDB?
+// import "gun/lib/store";
+
+
 
 let peers;
 
@@ -8,17 +16,20 @@ if (process.env.NODE_ENV === "development") {
 } else {
   peers = [
     // Community relay peers: https://github.com/amark/gun/wiki/volunteer.dht
-    "https://www.raygun.live/gun",
-    "https://gunmeetingserver.herokuapp.com/gun",
-    "https://gun-us.herokuapp.com/gun",
-    "https://gun-eu.herokuapp.com/gun",
+//     "https://www.raygun.live/gun",
+//     "https://gunmeetingserver.herokuapp.com/gun",
+//     "https://gun-us.herokuapp.com/gun",
+//     "https://gun-eu.herokuapp.com/gun",
     // My own relay peer
     // "https://phrassed.com/gun",
+    
   ];
 }
 
 const gun = new Gun({
   peers,
+  store: RindexedDB(),
+  localStorage: false,
 });
 
 // attaching gun to window for testing purposes
